db3817e615ca968af38b0a54061498673b8c2fcf
asmitacode <asmitac523@gmail.com>
2025-01-03 11:23:59 +0545

    question 5 finished

---
+++ question5.php
@@ -0,0 +1,7 @@
+<?php
+// Question 5: Calculate the area of a triangle
+function calculateTriangleArea($base, $height) {
+    return 0.5 * $base * $height; // Use the formula (base * height) / 2
+}
+echo calculateTriangleArea(10, 15); // Test the function with base 10 and height 15
+?>
