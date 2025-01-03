44a4498ef151e41a9db075388cd22ee46d1d81a0
asmitacode <asmitac523@gmail.com>
2025-01-03 11:16:47 +0545

    question 3 finished

---
+++ question3.php
@@ -0,0 +1,7 @@
+<?php
+// Question 3: Convert minutes to seconds
+function convertMinutesToSeconds($minutes) {
+    return $minutes * 60; // Multiply minutes by 60 to get seconds
+}
+echo convertMinutesToSeconds(5); // Test the function with 5 minutes
+?>
