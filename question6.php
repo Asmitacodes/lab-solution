a1dbe0e562615f4e7ceda33d78d930ebf4c0b756
asmitacode <asmitac523@gmail.com>
2025-01-03 11:26:41 +0545

    question 6 finished

---
+++ question6.php
@@ -0,0 +1,7 @@
+<?php
+// Question 6: Convert age in years to days
+function ageInDays($age) {
+    return $age * 365; // Multiply age in years by 365 to get days
+}
+echo ageInDays(25); // Test the function with age 25
+?>
