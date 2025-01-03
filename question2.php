4baf63c0f155f674735f89e59dd79682e64d5c33
asmitacode <asmitac523@gmail.com>
2025-01-03 11:09:31 +0545

    question 2 finished

---
+++ question2.php
@@ -0,0 +1,7 @@
+<?php
+// Question 2: Calculate the area of a circle
+define("PI", 3.14159); // Define a constant for the value of PI
+$radius = 5; // Define the radius of the circle
+$area = PI * $radius * $radius; // Calculate the area using the formula PI*r^2
+echo "Area of Circle: $area"; // Display the calculated area
+?>
