ce58f26fb903e43a4b0b630520ad44bc05b7c86c
asmitacode <asmitac523@gmail.com>
2025-01-03 11:21:19 +0545

    question 4 finished

---
+++ question4.php
@@ -0,0 +1,7 @@
+<?php
+// Question 4: Return the sum of two numbers
+function sum($a, $b) {
+    return $a + $b; // Add the two numbers and return the result
+}
+echo sum(5, 10); // Test the function with 5 and 10
+?>
